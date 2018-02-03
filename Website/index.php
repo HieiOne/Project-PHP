add34e3c73f125afb7a22cd3fe42526eebbf42ce
Ivan Blasco Gascon <blascogasconiban@gmail.com>
2018-02-03 21:42:14 +0100

    Added dropdown to login image (panel control, logout)

--- Website/index.php
+++ Website/index.php
@@ -35,7 +35,14 @@
             ?>
 
             <div class="top-bar-buttons">
-                <a href="login/login.php"><img class="top-bar-buttons-login" src="img/Login1.png" alt="Login"></a>                    
+                <div class="login-dropdown">
+                    <a href="login/login.php"><img class="top-bar-buttons-login" src="img/Login1.png" alt="Login"></a>
+                    <div class="login-dropdown-content">
+                        <a class="login-drop-content-links" href="login/login.php">Panel de Control</a>
+						<a class="login-drop-content-links" href="login/sessiondestroy.php">Logout</a>
+                    </div>
+                </div>
+
                 <a href="#"><img class="top-bar-buttons-cart" src="img/Cart1.png" alt="Cart"></a>                                   
             </div>
         </div>
@@ -45,8 +52,8 @@
         <!-- Menu bar -->
 
         <div class="menu-bar">
-		   <a href="#">
-                <div class="menu-bar-categories drop-menu">
+		   <a href="#"> <!-- Categories -->
+                <div class="menu-bar-categories drop-menu"> 
                     <img class="images-position" src="img/category_logo1.png" alt="Categories">
                     <p class="text-position">Categories</p>
 					<div class="drop-content">
@@ -57,21 +64,21 @@
                 </div>
 			</a>
 				
-            <a href="#">
+            <a href="#"> <!-- Promotions -->
                 <div class="menu-bar-categories">
                     <img class="images-position" src="img/promotion_logo1.png" alt="Promotions">
                     <p class="text-position">Promotions</p>                    
                 </div>
             </a>
             
-            <a href="#">
+            <a href="#"> <!-- New Books -->
                 <div class="menu-bar-categories">
                     <img class="images-position" src="img/newbooks_logo3.png" alt="New books">
                     <p class="text-position">New Books</p>     
                 </div>
             </a>
 
-            <a href="#">
+            <a href="#"> <!-- Social Network -->
                 <div class="menu-bar-categories">
                     <div class="images-position">
                         <a href="https://www.youtube.com"><img class="socialnetwork-images" src="img/logo/youtube.png" alt="Youtube"></a>
